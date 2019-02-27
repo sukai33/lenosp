c370fb960ac6dfcc8501cf668531d44cba8f0671
sukai <422168787@qq.com>
2019-02-27 14:50:38 +0800

    url

--- len-web/image_build.sh
+++ len-web/image_build.sh
@@ -3,10 +3,11 @@
 docker rmi len-web:1.0-SNAPSHOT
 docker rmi swr.cn-north-1.myhuaweicloud.com/lenosp/len-web:$1
 #将parent、model、utils、common基础工程打包，上传到maven本地仓库
-mvn -f ../len-activiti/pom.xml install
-mvn -f ../len-blog/pom.xml install
 mvn -f ../len-core/pom.xml install
 mvn -f ../len-sys/pom.xml install
+mvn -f ../len-blog/pom.xml install
+mvn -f ../len-activiti/pom.xml install
+mvn -f ../len-web/pom.xml install
 #使用maven插件构建docker镜像
 mvn -f pom_docker.xml clean package -DskipTests docker:build
 #修改镜像的组织名，以便推送到个人组织内，注意这里xc-edu-01是新添加的组织名
